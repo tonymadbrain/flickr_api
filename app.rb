05f3bd958faa7e4048b3e2aa69cca03a2721ceb2
Antony Ryabov <mb7222@yandex.ru>
2016-04-05 15:42:10 +0300

    Try disable https endpoints

--- app.rb
+++ app.rb
@@ -44,7 +44,8 @@ def timenow
 end
 
 #auth
-FlickRaw.check_certificate = false
+FlickRaw.secure = false
+# FlickRaw.check_certificate = false
 FlickRaw.api_key       = flickr_cred[:api_key]
 FlickRaw.shared_secret = flickr_cred[:shared_secret]
 flickr.access_token    = flickr_cred[:access_token]
