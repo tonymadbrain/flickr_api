24bdb921d9c6e1788cfea56c8e5f7c0d6f52c8a3
Antony Ryabov <mb7222@yandex.ru>
2016-04-05 15:38:18 +0300

    Fix typo

--- app.rb
+++ app.rb
@@ -44,7 +44,7 @@ def timenow
 end
 
 #auth
-FlickrRaw.check_certificate = false
+FlickRaw.check_certificate = false
 FlickRaw.api_key       = flickr_cred[:api_key]
 FlickRaw.shared_secret = flickr_cred[:shared_secret]
 flickr.access_token    = flickr_cred[:access_token]
